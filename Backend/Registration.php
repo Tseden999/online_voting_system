2735d26bac8a1e327e2ca4baa347d508151eeef4
Tseden999 <tsetendolma055@gmail.com>
2023-07-20 20:16:45 +0545

    validation

--- Backend/Registration.php
+++ Backend/Registration.php
@@ -1,5 +1,7 @@
 <?php
-include "DbConnection.php";
+ 
+ try{
+    include "DbConnection.php";
 
     $name= $_POST['username'];
     $age=$_POST['age'];
@@ -9,10 +11,16 @@ include "DbConnection.php";
     $password=$_POST['password'];
     $role=$_POST['role'];
 
-    $targetDir = "./uploads/";
+    //empty validation
+    if(strlen($name)===0 ||strlen($age)===0 ||strlen($address)===0 ||strlen($contact)===0 ||strlen($eamil)===0 ||strlen($password)===0 ||strlen($role)===0 ){
+        echo "Please fill up all the fields!!!";
+    }
+    else {
+        $targetDir = "./uploads/";
     $targetFile = $targetDir . basename($_FILES["profile"]["name"]);
 
     if (move_uploaded_file($_FILES["profile"]["tmp_name"], $targetFile)) {
+
         // Insert the file details into the database
         $fileName = basename($_FILES["profile"]["name"]);
         
@@ -26,6 +34,7 @@ include "DbConnection.php";
             $insert_query = "INSERT INTO admin( username, email, password, contact_no, age, address, profile) VALUES( '$name',' $email', '$password', '$contact', '$age','$address','$fileName')";
         }
         
+
         if($conn->query($insert_query) === TRUE){
         echo "Register successfull !";
         }else{
@@ -35,5 +44,11 @@ include "DbConnection.php";
         // Error uploading the file
         echo "Error uploading the file.";
     }
+    } 
+}
+
+catch (Exception $e){
+    echo 'Failed to register!!! ' .$e->getMessage();
+}
  
 ?>
